7b9c6e7b44b60035876ac4390db495e204c98e84
Yibin Wei <yibinwei@live.com>
2019-02-09 10:56:41 -0500

    remaking the repo so its less cluttered

---
+++ routes/items/historical.js
@@ -0,0 +1,50 @@
+const axios = require('axios');
+// Load the full build.
+const _ = require('lodash');
+// Load the core build.
+//const _ = require('lodash/core');
+// Load the FP build for immutable auto-curried iteratee-first data-last methods.
+const fp = require('lodash/fp');
+
+// function returnArray(jsonArray){
+//     data = [];
+//     for(var b=0 ; b < jsonArray.length ; b++){
+//         data[b] = {
+//             listing_datetime: jsonArray[b].listing_datetime.slice(0,10),
+//             unit_price: jsonArray[b].unit_price,
+//             quantity: jsonArray[b].quantity,
+//             listings: jsonArray[b].listings
+//         }
+//     }
+//     return data
+// }
+var getHighScores = function(data){
+    var sellDates = _.uniq(data.data.results.map((item)=> item.listing_datetime.slice(0,10))).slice(0, 31);
+
+    var sellResult = [];
+    var sellData = data.data.results;
+    sellDates.forEach((date)=>{
+
+        sellResult.push(sellData.filter((item)=>item.listing_datetime.slice(0,10) === date)[0]);
+    });
+   return sellResult;
+}
+module.exports = function (app) {
+    app.get('/api/history/:id',function(req,res){
+     
+
+        var buying = axios.get("http://www.gw2spidy.com/api/v0.9/json/listings/"+req.params.id+"/buy/1")
+        .then(getHighScores)
+        .catch(function(error){
+            console.log(error);
+            res.send("error");
+        });
+        var selling = axios.get("http://www.gw2spidy.com/api/v0.9/json/listings/"+req.params.id+"/sell/1")
+        .then(getHighScores)
+        .catch(function(error){
+            console.log(error);
+            res.send("error");
+        });
+        Promise.all([buying, selling]).then((values)=>res.send({buying: values[0], selling: values[1]} ));
+    });
+};
---
+++ routes/items/item.js
@@ -0,0 +1,54 @@
+const axios = require('axios');
+
+module.exports = function (app) {
+    app.get('/api/item/:id',function(req,res){
+        var itemData = {
+            id:0,
+            data:{
+                name:"",
+                level:0,
+                description:"",
+                icon:"",
+                restriction: []
+            },
+            pricing:{
+                buy:{
+                    quantity:0,
+                    price:0
+                },
+                sell:{
+                    quantity:0,
+                    price:0
+                }
+            }
+        };
+
+        axios.get("https://api.guildwars2.com/v2/items/"+req.params.id)
+        .then(function(response){
+            // console.log(response.data);
+            item = response.data;
+            itemData.id = item.id;
+            itemData.data.name = item.name;
+            itemData.data.description = item.description;
+            itemData.data.icon = item.icon;
+            itemData.data.level = item.level;
+            itemData.data.restriction = item.flags;
+        }).catch(function(error){
+            console.log("item data error:\n"+error);
+        })
+        .then(axios.get("https://api.guildwars2.com/v2/commerce/prices/"+req.params.id)
+        .then(function(response){
+            // console.log(response.data);
+            price = response.data;
+            itemData.pricing.buy.quantity = price.buys.quantity;
+            itemData.pricing.buy.price = price.buys.unit_price;
+            itemData.pricing.sell.quantity = price.sells.quantity;
+            itemData.pricing.sell.price = price.sells.unit_price;
+        }).catch(function(error){
+            console.log("item pricing error:\n"+error);
+        })
+        ).then(function(){
+            res.send(itemData);
+        });
+    });
+}
