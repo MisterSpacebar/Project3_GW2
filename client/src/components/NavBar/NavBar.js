9a4233e8a76a6771aefd0d2bf0be40d42f966f9a
Yibin Wei <yibinwei@live.com>
2019-02-21 02:21:13 -0500

    changed navbar to basic search form

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,25 +1,34 @@
 import React from "react";
-import {Navbar, NavItem, Autocomplete} from 'react-materialize';
+import {Navbar, NavItem, Input} from 'react-materialize';
 
-const navBar = () => (
-<Navbar brand='logo' right>
-  <Autocomplete
-    className='red lighten-5 black-text'
-    title='Example'
-    data={
-      {
-        'Apple': null,
-        'Microsoft': null,
-        'Google': 'http://placehold.it/250x250'
-      }
-    }
-  />
-<NavItem href='/' left>Home</NavItem>
-<NavItem href='/user'left>User</NavItem>
-<NavItem href='/about'left>About</NavItem>
-</Navbar> 
+class navBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {term : ''};
+  }
 
-);
-
+  render () {
+    return (
+      <Navbar brand='logo' right>
+        {/* <Autocomplete
+          className='red lighten-5 black-text'
+          title='Example'
+          data={
+            {
+              'Apple': null,
+              'Microsoft': null,
+              'Google': 'http://placehold.it/250x250'
+            }
+          }
+        /> */}
+      <Input value={this.state.term} onChange = {event => this.setState({term : event.target.value}) }/>
+      <NavItem href='/' left>Home</NavItem>
+      <NavItem href='/user'left>User</NavItem>
+      <NavItem href='/about'left>About</NavItem>
+      </Navbar> 
+      
+      )
+  }
+}
 
 export default navBar;
