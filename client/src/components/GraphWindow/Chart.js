871e45484ff560e8ab5a40898b686b51b3cdac08
Ksenia Kirilyuk <kseniakirilyuk@gmail.com>
2019-02-28 20:13:59 -0500

    Y code

--- client/src/components/GraphWindow/Chart.js
+++ client/src/components/GraphWindow/Chart.js
@@ -1,58 +1,70 @@
 import React, {Component} from 'react';
-import './Chart.css';
-import API from "../../utils/API";
+import './Chart.css'
 import {Line} from 'react-chartjs-2';
+// import '../../../../models'
+import API from '../../utils/API.js'
 
 class Chart extends Component {
     constructor(props){
         super(props);
         this.state={
-            chartData:{
-                labels:['B', 'W', 'S', 'L', 'C', 'NB'],
-                id:24,
-                datasets:[
-                    {
-                        label:'Boots',
-                        borderColor:'rgba(255, 102, 102, 0.6)',
-                        backgroundColor:'rgba(255, 102, 102, 0.6)',
-                        data:[],
-                        yAxisId:'y-axis-1'}
-                    // }, {
-                    //     label:'Hat',
-                    //     borderColor:'rgba(204, 153, 255, 0.6)',
-                    //     backgroundColor:'rgba(204, 153, 255, 0.6)',
-                    //     data:[
-                    //         187651,
-                    //         123456,
-                    //         175467,
-                    //         268576,
-                    //         276598,
-                    //         367456
-                    //     ],
-                    //     yAxisId:'y-axis-2',
-                    // }
-                ]
-            }
-        }
+            id: 90011,
+            name: "",
+            img:"",
+            chartData:{}
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
-    componentDidMount() {
+    // fires after successful mount
+    componentDidMount(){
+        
+    }
+    componentDidUpdate(){
+
+    }
+    handleSubmit(e){
+        e.preventDefault();
+        const value = this.state.id
+
+        API.getHistory(value)
+        .then(response => {
+            console.log(response.data.data);
+            console.log(response.data.chartData);
+            // this.setState({chartData:response.data.chartData});
+            console.log(response.data.name);
+            // this.setState({name:response.data.name});
+            // this.setState({img:response.data.icon});
+            this.setState({
+                chartData:response.data.chartData,
+                name:response.data.name,
+                img:response.data.icon
+            });
+        })
     };
-    loadItemHistory = id => {
-      console.log('i got data for id ', id)
-      API.getHistory(id)
-        .then(res =>
-          this.setState({ data: res.data.data})
-        )
-        .catch(err => console.log(err));
+    handleChange(e){
+        e.preventDefault();
+        const target = e.target;
+        const value = target.value;
+
+        this.setState({
+            id: value
+        })
     };
     render(){
         return(
             <div className="Chart col s12">
-               <h2>Graph Name</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <input type='text' name="serach" value={this.state.id} onChange={this.handleChange} />
+                    <input type="submit" name="searchButton" value="Submit" />
+                </form>
+                <img src={this.state.img}></img>
+                <h4>{this.state.name}</h4>
                 <Line
                     data={this.state.chartData}
                     options={{}}
                 />
+
             </div>
         )
     }
