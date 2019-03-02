fc370febcd52d894a80bb6aabadf2c97bc707a8d
Yibin Wei <yibinwei@live.com>
2019-03-02 16:17:52 -0500

    working on pricing API, still issues

--- routes/items/pricing.js
+++ routes/items/pricing.js
@@ -6,11 +6,12 @@ var getHighScores = function(data){
     var sellDates = _.uniq(data.data.results.map((item)=> item.listing_datetime.slice(0,10))).slice(0, 31);
 
     var sellResult = [];
+
     var sellData = data.data.results;
     sellDates.forEach((date)=>{
         sellResult.push(sellData.filter((item)=>item.listing_datetime.slice(0,10) === date)[0]);
     });
-   return sellResult.slice(0,2);
+    return sellResult.slice(0,2);
 }
 
 module.exports = function (app) {
@@ -28,16 +29,85 @@ module.exports = function (app) {
         //     res.send("error");
         // });
 
+        var resultOrder = [];
+
         var bestFunction = besturls.map(function(element){
             return axios.get(element)
                 .then(getHighScores)
+                .then(resultOrder.push(element.slice(47,element.length-7)))
                 .catch(function(error){
                 console.log(element+" error: "+error);
             });
         });
 
-        Promise.all([bestFunction]).then(function(values){
-            console.log(values);
-        })
+        Promise.all(bestFunction).then(function(values){
+            // values.forEach(function(element){
+
+            // })
+            // console.log(values);
+            console.log("order "+resultOrder);
+            result = resultOrder.map(function(element){
+                return parseInt(element)
+            })
+            console.log("int version of order: "+result);
+
+            var thing = {
+                id: 0,
+                name: "",
+                price: 0,
+                difference: 0,
+            };
+
+            var things = [];
+
+            values.forEach(function(element){
+                console.log(element[0]);
+                resultOrder.forEach(function(ele){
+                    axios.get("https://api.guildwars2.com/v2/items/"+ele)
+                    .then(function(response){
+                        thing.id = ele;
+                        thing.name = response.data.name;
+                        thing.price = element[0].unit_price;
+                        thing.difference = (element[0].unit_price-element[1].unit_price);
+
+                        console.log(thing)
+
+                        things.push({
+                            id: ele,
+                            name: response.data.name,
+                            price: element[0].unit_price,
+                            difference:(element[0].unit_price-element[1].unit_price)
+                        });
+
+                        console.log(things)
+                    }).catch(function(error){
+                        console.log(error);
+                        res.send("error");
+                    })
+                });
+            }).then(function(){
+                res.send(things);
+            }).catch(function(error){
+                console.log(error)
+            })
+
+            values.forEach(function(element){
+                thing.price = element[0].unit_price;
+                thing.difference = (element[0].unit_price-element[1].unit_price);
+            })
+            resultOrder.forEach(function(ele){
+                axios.get("https://api.guildwars2.com/v2/items/"+ele)
+                .then(function(response){
+                    thing.id = ele;
+                    thing.name = response.data.name;
+
+                }).catch(function(error){
+                    console.log(error);
+                    res.send("error");
+                })
+            })
+
+            
+        });
     });
 }
