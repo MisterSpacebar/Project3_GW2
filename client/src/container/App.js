e2fb6e3431c3667a5f38b84c270bb9d385fd3c50
Eddie Vilchez <eddie.vilchez15@gmail.com>
2019-03-05 17:30:09 -0500

    adding uauth files

--- client/src/container/App.js
+++ client/src/container/App.js
@@ -7,8 +7,69 @@ import Chart from '../components/GraphWindow/Chart';
 import UserFavourites from '../components/UserFavourites/UserFavourites';
 import Currency from '../components/Currency/Currency';
 import ItemBestWrapper from '../components/ItemBestWrapper/ItemBestWrapper'
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
 
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from './components/layout/Navbar';
+import Home from './components/pages/Home';
+import Users from './components/pages/Users';
+import Login from './components/auth/Login';
+
+
+function onAuthRequired({ history }) {
+
+  history.push('/login');
+
+}
+
+class App extends Component {
+
+  render() {
+
+    return (
+
+      <Router>
+        <Security issuer="https://dev-866123.okta.com/oauth2/default"
+                  client_id="0oabww280sGNf4lia356"
+                  redirect_uri={window.location.origin + '/implicit/callback'}
+                  onAuthRequired={onAuthRequired}>
+        <div className="App">
+
+            <Navbar />
+            <div className="container">
+
+              <Route path="/" exact={true} component={Home} />
+
+              <SecureRoute path="/staff" exact={true} component={Staff} />
+
+              <Route
+
+                path="/login"
+
+                render={() => (
+
+                  <Login baseUrl="https://dev-409495.oktapreview.com" />
+
+                )}
+
+              />
+
+              <Route path="/implicit/callback" component={ImplicitCallback} />
+
+            </div>
+
+          </div>
+
+        </Security>
+
+      </Router>
+
+    );
+
+  }
+
+}
 
 class App extends Component {
   // state = {
